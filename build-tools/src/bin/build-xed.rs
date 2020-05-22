305e55a0e03e6f551879e08aec4ccb735b78816f
Phantomical <phantom@lynches.ca>
2020-05-22 16:51:35 -0400

    Tweak build scripts to work when python executable is broken
    By default on Win10 there is a python3 executable but all it
    does is print something along the lines of "Get Python 3 from
    the windows store" and then error.
    
    To fix this, we opt to ignore a python installation if
    `python[3] -V` returns an error.

--- build-tools/src/bin/build-xed.rs
+++ build-tools/src/bin/build-xed.rs
@@ -2,7 +2,10 @@ use std::{
     env, fs, io,
     path::{Path, PathBuf},
     process::Command,
+    str::FromStr,
 };
+
+use target_lexicon::Triple;
 
 fn create_dir<P: AsRef<Path>>(path: P) -> io::Result<()> {
     fs::create_dir(path).or_else(|e| match e.kind() {
@@ -11,22 +14,27 @@ fn create_dir<P: AsRef<Path>>(path: P) -> io::Result<()> {
     })
 }
 
+fn find_python() -> &'static str {
+    if let Ok(status) = Command::new("python3").arg("-V").status() {
+        if status.success() {
+            return "python3";
+        }
+    }
+
+    if let Ok(status) = Command::new("python").arg("-V").status() {
+        if status.success() {
+            return "python";
+        }
+    }
+
+    panic!("Unable to find a working python installation");
+}
+
 fn main() {
     let out_dir = PathBuf::from(env::var("OUT_DIR").expect("Failed to read OUT_DIR"));
     let cwd = env::current_dir().expect("Failed to get CWD");
+    let target = env::var("TARGET").expect("Failed to read TARGET");
     let profile = env::var("PROFILE").unwrap_or_else(|_| "debug".to_owned());
-
-    // Ensure python exists and can run
-    match Command::new("python").arg("-V").output() {
-        Ok(output) => {
-            if !output.status.success() {
-                panic!("'python -V' returned with an error");
-            }
-        }
-        Err(e) => {
-            panic!("Python is required to run xed: {}", e);
-        }
-    }
 
     let install_dir = out_dir.join("install");
     let build_dir = out_dir.join("build");
@@ -47,18 +55,29 @@ fn main() {
     env::set_current_dir(&build_dir).unwrap();
 
     let num_jobs: u32 = env::var("NUM_JOBS")
-        .unwrap_or("1".to_owned())
+        .unwrap_or_else(|_| "1".to_owned())
         .parse()
         .unwrap_or(1);
 
-    let mut cmd = Command::new("python");
-    cmd.arg(&mfile_path)
+    let python = find_python();
+    let mut cmd = Command::new(python);
+    cmd
+        // The -B flag prevents python from generating .pyc files
+        .arg("-B")
+        .arg(&mfile_path)
         .arg(format!("--jobs={}", num_jobs))
         .arg("--silent")
         .arg("--static-stripped")
         .arg("--extra-ccflags=-fPIC")
         .arg("--no-werror")
-        .arg(format!("--install-dir={}", install_dir.display()));
+        .arg(format!(
+            "--host-cpu={}",
+            Triple::from_str(&target)
+                .expect("TARGET was not a valid target triple")
+                .architecture
+        ))
+        .arg(format!("--install-dir={}", install_dir.display()))
+        .env("PYTHONDONTWRITEBYTECODE", "x");
 
     if profile == "release" {
         cmd.arg("--opt=3");
@@ -68,9 +87,13 @@ fn main() {
 
     cmd.arg("install").current_dir(&build_dir);
 
+    eprintln!("XED build command: {:?}", cmd);
+
     let status = cmd.status().expect("Failed to start xed build");
 
     if !status.success() {
         panic!("Building xed failed");
     }
+
+    let lib_dir = install_dir.join("lib");
 }
