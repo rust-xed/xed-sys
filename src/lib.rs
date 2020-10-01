df5f9f5f60f48a10f463210793f651e2412b5ece
Phantomical <phantom@lynches.ca>
2020-10-01 16:29:43 -0400

    Fix warning due to nonexistant warning allow
    Looks like broken_intra_doc_links only exists on nightly at this
    point so we get an error if we try to allow it.

--- src/lib.rs
+++ src/lib.rs
@@ -79,7 +79,7 @@ mod _detail {
             non_camel_case_types,
             non_snake_case,
             non_upper_case_globals,
-            broken_intra_doc_links
+            intra_doc_link_resolution_failure
         )]
 
         include!(concat!(env!("OUT_DIR"), "/xed_interface.rs"));
